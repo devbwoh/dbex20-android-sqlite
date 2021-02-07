8e05d207041e812c79b8d8ed7df605a734505dc5
bwoh <bwohkit@gmail.com>
2021-02-08 01:59:43 +0900

    ➕ 데이터 추가 UI 완성

--- app/src/main/java/kr/ac/kumoh/s20210000/gunplasqliteapplication/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20210000/gunplasqliteapplication/MainActivity.kt
@@ -3,8 +3,13 @@ package kr.ac.kumoh.s20210000.gunplasqliteapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import kr.ac.kumoh.s20210000.gunplasqliteapplication.databinding.ActivityMainBinding
+import kr.ac.kumoh.s20210000.gunplasqliteapplication.databinding.DialogAddBinding
+
 // 본인의 package 사용할 것
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +22,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.fab.setOnClickListener { addItem() }
+
 //        model.insert(Mechanic(0,
 //            "건담",
 //            "RX-78-2",
@@ -24,5 +31,28 @@ class MainActivity : AppCompatActivity() {
 //            "Luna Titanium",
 //            18.0,
 //            43.4))
+    }
+
+    private fun addItem() {
+        val binding = DialogAddBinding.inflate(layoutInflater)
+        AlertDialog.Builder(this)
+            .setView(binding.root)
+            .setPositiveButton("추가") { _, _ ->
+                val name = binding.name.text.toString()
+                Log.i("Dialog", name)
+
+                if (binding.name.text.toString().isNotEmpty() &&
+                    binding.model.text.toString().isNotEmpty())
+                    model.insert(Mechanic(
+                        0,
+                        binding.name.text.toString(),
+                        binding.model.text.toString(),
+                        binding.manufacturer.text.toString(),
+                        binding.armor.text.toString(),
+                        binding.height.text.toString().toDouble(),
+                        binding.weight.text.toString().toDouble()
+                    ))
+            }
+            .create().show()
     }
 }
